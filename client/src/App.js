c7d4bb11ec078343b1a1b360dbf501e12952e7ce
Emmanuel <emmanuelbboateng@yahoo.com>
2022-08-23 16:22:29 -0400

    adding packs

--- client/src/App.js
+++ client/src/App.js
@@ -32,7 +32,7 @@ function App() {
     return (
       <div className="App" style={{ backgroundColor: "lightgrey" }}>
         
-        {/* <NavBar setLoggedInUser={setLoggedInUser}/> */}
+      
 
       <Routes>
         <Route path="/" element={<Home />} />
